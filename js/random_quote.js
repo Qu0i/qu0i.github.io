a1600ac6c206b4997201f03a38a842ca0c9afe98
Qu0i <quoiteam.com@gmail.com>
2026-05-16 15:40:14 +0300

    [Update] Projects data, quotes, homepage info

--- js/random_quote.js
+++ js/random_quote.js
@@ -1,6 +1,8 @@
 const quotes = [
   "“The more I learn, the more I realize how much I don't know.” — Albert Einstein",
+  "“Unpin your butterflies, Russia.” — ∆",
   "“Hell is other people.” — Jean-Paul Sartre",
+  "“Triangles are my favourite shape.” — ∆",
   "“I think, therefore I am.” — René Descartes",
   "“The fear has gripped me but here I go.” — ∆",
   "“Curiosity is the beginning of understanding.” — Neil deGrasse Tyson",
